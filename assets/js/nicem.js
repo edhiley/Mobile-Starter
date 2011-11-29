ff2fcfdfe7bd3e29b0545f54432598e4e38f3d7f
Edward Hiley <edwardhiley@unknown-00-1e-c2-ae-8d-1d.home>
2011-11-29 00:53:54 +0000

    syndication 101 (ish)

--- assets/js/nicem.js
+++ assets/js/nicem.js
@@ -5,6 +5,9 @@ var nicem = (function () {
 	nicem.init = function () {
         setupPageLoadEvent();
         nicem.subscribeToPageLoad("pageLoad", ajaxPageLoad);
+
+		$.mobile.loadingMessage = "searching..."
+		$('#searchbutton').click(performSearch);
     };
 
     nicem.subscribeToPageLoad = function (name, func) {
@@ -12,6 +15,40 @@ var nicem = (function () {
             pageLoad[name] = func;
         }
     };
+
+	function performSearch() {
+		
+		var url = "https://sandbox-syndication.evidence.nhs.uk/services/search/results?q=" + $('#search').val();
+		
+		$.ajax({
+		        url: url,
+				beforeSend: function(xhr) {
+					xhr.setRequestHeader('Accept', 'application/json');
+					xhr.setRequestHeader('API-Key', '5a7a8af2-4084-4632-ba1d-556946e63363');
+				},
+	            success: function(data, textStatus, jqXhr) {
+	                
+					$('#searchresults').empty();
+					
+					var addDocument = function(doc) {
+						
+						var template = "<li data-theme=\"c\" class=\"ui-li ui-li-static ui-body-a\">" + 
+							"<a href=" + doc.Url + " data-transition=\"pop\" class=\"ui-link-inherit\">" +
+							"<h3 class=\"ui-li-heading\">" + doc.Title + "</h3>" +
+							"<p class=\"ui-li-desc\">" + doc.Abstract + "</p>" +
+						"</a><span class=\"ui-icon ui-icon-arrow-r ui-icon-shadow\"></span></div></li>";
+						
+						$('#searchresults').append(template);
+					};
+	
+					jQuery.each(data.Documents,function(){
+						addDocument(this);
+					});
+	            }
+	        });
+		
+		return false;
+	}
 
     function setupPageLoadEvent () {
         $('div[data-role="page"]').live('pagebeforecreate', function() {
